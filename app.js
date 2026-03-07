bc541ac3920e2f03e000492f2fdf24883347c989
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2026-03-07 09:56:42 +0000

    routes skeleton

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import { PORT } from './config/env.js';
+import authRouter from './routes/auth.routes.js';
+import userRouter from './routes/user.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
 
 const app = express();
+app.use(express.json());
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
 
 app.get('/', (req, res) => {
     res.send('Welcome to Subscription Tracker API!');
-})
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
-})
+});
