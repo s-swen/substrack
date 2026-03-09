b92534e069fc053fe2e0bb3cf41ab26c43317e2f
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2026-03-09 08:27:14 +0000

    working on signUp

---
+++ controllers/auth.controller.js
@@ -0,0 +1,12 @@
+
+export const signUp = async (req, res, next) => {
+
+};
+
+export const signIn = async (req, res, next) => {
+    
+};
+
+export const signOut = async (req, res, next) => {
+    
+};
